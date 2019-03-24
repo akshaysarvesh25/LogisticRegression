d0d881e9f2e6495efe6fdd18216156e800a1cd10
akshays25 <akshays25@tamu.edu>
2019-03-24 11:32:34 -0500

    Working copy of Logistic Regression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,6 +5,31 @@ from operator import itemgetter
 import matplotlib.pyplot as plt
 
 
+class DataGet:
+    def __init__(self,sample_train,sample_test):
+        self.x_train_path = sample_train
+        self.x_test_path  = sample_test
+
+    def GetSamples(self):
+        x_train = np.genfromtxt(self.x_train_path,delimiter=',')
+        x_train = np.array(x_train)
+        x_test = np.genfromtxt(self.x_test_path,delimiter=',')
+        x_test = np.array(x_test)
+        return x_train,x_test
+
+    def CollateData(self):
+        x_train,x_test = self.GetSamples()
+        y_train =  x_train[:,(x_train.shape[1] - 1)]
+        y_test = x_test[:,(x_test.shape[1] - 1)]
+        x_train_ip = x_train[:,0:(x_train.shape[1] - 1)]
+        x_test_ip  = x_test[:,0:(x_test.shape[1] - 1)]
+        y_test = np.array(y_test).reshape(-1,1)
+        y_train = np.array(y_train).reshape(-1,1)
+        new_col = x_test_ip.sum(1)[...,None]
+        x_test_ip = np.hstack((x_test_ip, new_col))
+        return x_train_ip,y_train,x_test_ip,y_test
+
+
 def RandomizedTrainingDataGenerator(x_train_ip,y_train_ip,NumbSamples):
     Samples_pos1 = np.array(random.sample(range(0, len(x_train_ip)/2), NumbSamples))
     Samples_neg1 = np.array(random.sample(range(len(x_train_ip)/2,len(x_train_ip)), NumbSamples))
@@ -21,7 +46,6 @@ def GeneratePredictor(x_train_rand1,y_train_rand1):
 
 def PredictOutput(reg1,x_test1):
     y_predicted = reg1.predict(x_test1)
-    #y_predicted = [-1 if (x<=0) else 1 for x in y_predicted]
     return y_predicted
 
 def OutputError(y_predict1,y_test1):
@@ -30,62 +54,42 @@ def OutputError(y_predict1,y_test1):
     return (y_predict1 != y_test1).sum()
 
 
+def main():
+    InputPath_trainingSet = '../Normal_train_10D.txt'
+    InputPath_testingSet = '../Normal_test_10D.txt'
+    DataObj = DataGet(InputPath_trainingSet,InputPath_testingSet)
 
-x_train = np.genfromtxt('../Uniform_train.txt',delimiter=',')
-x_train = np.array(x_train)
+    x_train_ip,y_train,x_test_ip,y_test = DataObj.CollateData()
 
-x_test = np.genfromtxt('../Uniform_test.txt',delimiter=',')
-x_test = np.array(x_test)
-
-
-
-y_train =  [sub_list[2] for sub_list in x_train]
-x_train_1 = [[sub_list[0]] for sub_list in x_train]
-x_train_2 = [[sub_list[1]] for sub_list in x_train]
-
-y_test = [sub_list[2] for sub_list in x_test]
-x_test_1 = [[sub_list[0]] for sub_list in x_test]
-x_test_2 = [[sub_list[1]] for sub_list in x_test]
-
-
-x_train_ip = np.concatenate((x_train_1,x_train_2),axis=1)
-
-x_test_ip  = np.concatenate((x_test_1,x_test_2),axis=1)
-new_col = x_test_ip.sum(1)[...,None]
-x_test_ip = np.hstack((x_test_ip, new_col))
-
-
-
-Train_data_numbers = [20, 50, 75, 100,250, 500]
-error =  np.zeros(len(Train_data_numbers))
-error_ = []
-iterations = 1000
-for iters in range(1,iterations):
-
-    for Train_data_numbers_ in Train_data_numbers:
-
-        x_train_rand,y_train_rand = RandomizedTrainingDataGenerator(x_train_ip,y_train,Train_data_numbers_)
-        reg = GeneratePredictor(x_train_rand,y_train_rand)
-        y_predict = PredictOutput(reg,x_test_ip)
-        error_.append(OutputError(y_predict,y_test))
-
-    #print(error_)
-    #error = error_
-    error = np.vstack((error,error_))
-
-
-
+    Train_data_numbers = [20, 50, 75, 100,250, 500]
+    error =  np.zeros(len(Train_data_numbers))
     error_ = []
+    iterations = 1000
 
-error = np.array(error)
-#print(error[100-2])
-error = np.sum(error, axis=0)
-error = error/iterations
+    for iters in range(1,iterations):
 
-plot1,= plt.plot(Train_data_numbers,error,'b.',label='Logistic regression error')
-plt.grid()
-plt.xlabel('Samples')
-plt.ylabel('Training error')
-plt.title('Training error for Logistic Regression')
-plt.legend()
-plt.show()
+        for Train_data_numbers_ in Train_data_numbers:
+
+            x_train_rand,y_train_rand = RandomizedTrainingDataGenerator(x_train_ip,y_train,Train_data_numbers_)
+            reg = GeneratePredictor(x_train_rand,y_train_rand)
+            y_predict = PredictOutput(reg,x_test_ip)
+            error_.append(OutputError(y_predict,y_test))
+
+
+        error = np.vstack((error,error_))
+        error_ = []
+
+    error = np.array(error)
+    error = np.sum(error, axis=0)
+    error = error/iterations
+
+    plot1,= plt.plot(Train_data_numbers,error,'b.',label='Logistic regression error')
+    plt.grid()
+    plt.xlabel('Samples')
+    plt.ylabel('Training error')
+    plt.title('Training error for Logistic Regression')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
